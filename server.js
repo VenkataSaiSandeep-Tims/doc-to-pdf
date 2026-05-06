d0d5e491e6de84d4848b6022a20c1226bcf71467
Muthavarapu Venkata Sai Sandeep <muthavarapuvenkatasai@tims.group>
2026-05-06 07:00:03 -0500

    added start file

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.post("/convert-docx", upload.single("file"), async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("✅ Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
